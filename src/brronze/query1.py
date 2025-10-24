2f033787653ca770f8e47258cd22320c11179d8d
stevenglover <steve.glover@outlook.com>
2025-10-24 19:48:57 -0400

    (mod) bronze/query1 - expanded query

--- src/brronze/query1.py
+++ src/brronze/query1.py
@@ -5,10 +5,21 @@
 
 # COMMAND ----------
 
-test = spark.read.table("mlops_dev.marvel_characters.test_set")
-train = spark.read.table("mlops_dev.marvel_characters.train_set")
+from databricks.connect import DatabricksSession
+from IPython.display import display
+
+# Fully declare spark session
+spark = DatabricksSession.builder.getOrCreate()
+
+# Testing Connection
+spark.sql("DROP TABLE IF EXISTS mlops_dev.bronze.marvel_characters")
+
+test = spark.read.table("mlops_dev.marvel_characters.test_set")  
+train = spark.read.table("mlops_dev.marvel_characters.train_set") 
 df = train.union(test)
-# display(df)
+display(df) 
 df.write.mode("overwrite").saveAsTable("mlops_dev.bronze.marvel_characters")
+
+
 
 # COMMAND ----------
