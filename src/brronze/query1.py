4069d50e6d962f5e7c7400bc3324ce944750685d
stevenglover <steve.glover@outlook.com>
2025-10-23 21:29:16 -0400

    (add) basic query to silver

--- src/brronze/query1.py
+++ src/brronze/query1.py
@@ -8,5 +8,7 @@
 test = spark.read.table("mlops_dev.marvel_characters.test_set")
 train = spark.read.table("mlops_dev.marvel_characters.train_set")
 df = train.union(test)
-display(df)
-# df.write.mode("overwrite").saveAsTable("mlops_dev.bronze.marvel_characters")
+# display(df)
+df.write.mode("overwrite").saveAsTable("mlops_dev.bronze.marvel_characters")
+
+# COMMAND ----------
---
+++ src/silver/query1.py
@@ -0,0 +1,10 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Simple Silver Query
+# MAGIC ***
+# COMMAND ----------
+silver = spark.read.table("mlops_dev.bronze.marvel_characters")
+lower_cols = [col.lower() for col in silver.columns]
+silver = silver.toDF(*lower_cols)
+silver.write.mode("overwrite").saveAsTable("mlops_dev.silver.marvel_characters")
+# COMMAND ----------
